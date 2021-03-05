7975090a9d9891909a240a19fda714343190b4b9
bang <yuudachi31@gmail.com>
2021-03-05 19:27:05 +0800

    first_push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './Sty.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,111 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      
+      <div class="page2">
+            <div class="body2">
+               <div class="a23sq">
+                  <div class="scrollsa skrollable " style={ {opacity: 1},{ top:-100+'px'},{filter:' blur(0px)'}}>
+                       <div >
+                        <img class="a23img" src="/images/a23.jpg"  />
+                     </div>
+                     </div>
+                     </div>
+                     <div class="blackh2">B L A C K </div> <div class="blackh2">H O L E</div>
+         
+           <div class="body3">
+              <div class="headr"></div>
+              <section1 class="sct1">
+                 <div class="abusq">
+                    <h1 class="abu">About Us</h1>
+                    <div class="abua">Black holes have always fascinated many
+                        people, and we introduce black holes and provide
+                        information about black holes on this site.</div>
+                  </div>
+            <div class="ppp"><iframe src="https://giphy.com/embed/OANp03XvXEV9u"  width="400" height="400" frameBorder="0" class="giphy-embed" allowFullScreen></iframe> </div>
+            
+         
+         </section1>
+         <section2>
+               <div class="ssqq1"> 
+         <div class="wsba">What's a black hole?</div>
+         
+         
+         <div class="contr">
+            
+            <div class="spr2">
+<div class="bkh"></div>
+            <div class="spr">
+                
+                <div class="sq1 sq"></div>
+                <div class="sq2 sq"></div>
+                <div class="sq3 sq"></div>
+                <div class="sq4 sq"></div>
+                <div class="sq5 sq"></div>
+                <div class="sq6 sq"></div>
+                <div class="sq7 sq"></div>
+                <div class="sq8 sq"></div>
+                <div class="sq9 sq"></div>
+                <div class="sq10 sq"></div>
+                <div class="sq11 sq"></div>
+                <div class="sq12 sq"></div>
+                <div class="sq13 sq"></div>
+                <div class="sq14 sq"></div>
+                <div class="sq15 sq"></div>
+                <div class="sq16 sq"></div>
+                <div class="sq17 sq"></div>
+                <div class="sq18 sq"></div>
+                <div class="sq19 sq"></div>
+                <div class="sq20 sq"></div>
+                <div class="sq21 sq"></div>
+                <div class="sq22 sq"></div>
+                <div class="sq23 sq"></div>
+                <div class="sq24 sq"></div>
+                <div class="sq25 sq"></div>
+                <div class="sq26 sq"></div>
+                <div class="sq27 sq"></div>
+                <div class="sq28 sq"></div>
+                <div class="sq29 sq"></div>
+                <div class="sq30 sq"></div>
+                <div class="sq31 sq"></div>
+                </div>
+            </div>    
+        </div>
+      
+          </div>
+          
+          
+          
+          </section2>
+          <section3>
+               <div class="ssqq">
+               <div class="wsba">Types of black hole</div>
+               
+              <img src="images/bling.png"class="bling"/>
+               
+               <iframe  src="https://giphy.com/embed/jEJYhqfTV2euA" width="400" height="396vw" class="dogs " frameBorder="0"  allowFullScreen></iframe>
+               </div>
+               
+          </section3>
+         <section4>
+               <div class="ssqq">
+               <div class="wsba2">How to find black holes?</div>
+               <div class="star"><img src="images/stars.jpg"/>
+                  </div>
+               
+
+                  </div>
+         </section4>
+        
+         <div class="scrollsa skrollable cam " style={{ opacity: 0},{filter: 'blur(0px)'}}>
+        <img src="images/cam.png"/>
+         </div>
+         
+         
+      </div>
+    </div>
+    </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { $ }  from 'react-jquery-plugin'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +16,60 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+$('.contr').hover(
+	function(){
+		$('.bkh').css('opacity','1');
+		$('.spr').css('transform','rotateZ(35765deg)');
+		$('.spr2').css('transform','rotateX(75deg)');
+		$('.bkh').css('transform','rotateX(-75deg)');
+		$('.sq1').css('width','10px').css('height','10px');
+		$('.sq2').css('width','10px').css('height','10px');
+		$('.sq3').css('width','10px').css('height','10px');
+		$('.sq4').css('width','10px').css('height','10px');
+		$('.sq5').css('width','10px').css('height','10px');
+		$('.sq6').css('width','10px').css('height','10px');
+		$('.sq7').css('width','10px').css('height','10px');
+		$('.sq8').css('width','10px').css('height','10px');
+		$('.sq9').css('width','10px').css('height','10px');
+		$('.sq10').css('width','10px').css('height','10px');
+		$('.sq11').css('width','10px').css('height','10px');
+		$('.sq12').css('width','10px').css('height','10px');
+		$('.sq13').css('width','10px').css('height','10px');
+		$('.sq14').css('width','10px').css('height','10px');
+		$('.sq15').css('width','10px').css('height','10px');
+		$('.sq16').css('width','10px').css('height','10px');
+		$('.sq17').css('width','10px').css('height','10px');
+		$('.sq18').css('width','10px').css('height','10px');
+		$('.sq19').css('width','10px').css('height','10px');
+		$('.sq20').css('width','10px').css('height','10px');
+		$('.sq21').css('width','10px').css('height','10px');
+		$('.sq22').css('width','10px').css('height','10px');
+		$('.sq23').css('width','10px').css('height','10px');
+		$('.sq24').css('width','10px').css('height','10px');
+		$('.sq25').css('width','10px').css('height','10px');
+		$('.sq26').css('width','10px').css('height','10px');
+		$('.sq27').css('width','10px').css('height','10px');
+		$('.sq28').css('width','10px').css('height','10px');
+		$('.sq29').css('width','10px').css('height','10px');
+		$('.sq30').css('width','10px').css('height','10px');
+		$('.sq31').css('width','10px').css('height','10px');
+	
+	},function(){
+		$('.bkh').css('opacity','0');
+		$('.spr').css('transform','rotateZ(-10deg)');
+		$('.spr2').css('transform','rotateX(-15deg)');
+		$('.bkh').css('transform','rotateX(0deg)');
+		$('.sq31').css('width','100px').css('height','100px');
+	
+	}
+	
+	
+	)
+$('.bling').hover(
+	function(){
+		$('.bling').css('transform','rotateZ(888888deg)');
+	}
+	,function(){
+		$('.bling').css('transform','rotateZ(-8000deg)');
+	}
+)
